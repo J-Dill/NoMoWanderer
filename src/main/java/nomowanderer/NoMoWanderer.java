198093a1e238fd84818e93089ec586d5fd1d0d88
Dylan <dylan.jech@mnsu.edu>
2019-10-26 22:44:29 -0500

    Block is now loading with a texture

--- src/main/java/nomowanderer/NoMoWanderer.java
+++ src/main/java/nomowanderer/NoMoWanderer.java
@@ -3,6 +3,7 @@ package nomowanderer;
 import com.google.common.base.Preconditions;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -10,17 +11,22 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.common.extensions.IForgeContainerType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.IForgeRegistryEntry;
+import net.minecraftforge.registries.ObjectHolder;
 import nomowanderer.blocks.ExampleTileEntityBlock;
 import nomowanderer.tileentity.ExampleTileEntityTileEntity;
+import nomowanderer.tileentity.ExampleTileEntityTileEntityRenderer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -33,9 +39,10 @@ public class NoMoWanderer {
     public static final String MODID = "nomowanderer";
 
     private static final Logger LOGGER = LogManager.getLogger(NoMoWanderer.MODID + " Mod Event Subscriber");
-
+    @ObjectHolder(MODID + ":" + ExampleTileEntityTileEntity.ID)
     public static TileEntityType<ExampleTileEntityTileEntity> EXAMPLE_TILE_ENTITY;
-    private static final Block EXAMPLE_BLOCK = new ExampleTileEntityBlock();
+    @ObjectHolder(MODID + ":" + ExampleTileEntityBlock.ID)
+    private static Block EXAMPLE_BLOCK = new ExampleTileEntityBlock();
     private static Block[] blocks = new Block[]{EXAMPLE_BLOCK};
 
     public NoMoWanderer() {
@@ -43,6 +50,15 @@ public class NoMoWanderer {
         MinecraftForge.EVENT_BUS.register(this);
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 //        modEventBus.addListener(this::onRegisterBlocks);
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void clientSetup(FMLClientSetupEvent event) {
+//            ScreenManager.registerFactory(PedestalContainer.TYPE, PedestalScreen::new);
+            ClientRegistry.bindTileEntitySpecialRenderer(ExampleTileEntityTileEntity.class, new ExampleTileEntityTileEntityRenderer());
+        }
     }
 
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -57,10 +73,14 @@ public class NoMoWanderer {
 //            event.getRegistry().register(IForgeContainerType.create(((windowId, inv, data) -> new PedestalContainer(windowId, inv, data.readBlockPos()))).setRegistryName(MODID, "pedestal"));
 //        }
 
-//        @SubscribeEvent
-//        public static void onRegisterTEType(RegistryEvent.Register<TileEntityType<?>> event) {
-//            event.getRegistry().register(TileEntityType.Builder.create(Example::new, PEDESTAL_BLOCK).build(null).setRegistryName(new ResourceLocation(MODID, "pedestal")));
-//        }
+        @SubscribeEvent
+        public static void onRegisterTEType(RegistryEvent.Register<TileEntityType<?>> event) {
+            event.getRegistry().register(
+                    TileEntityType.Builder.create(
+                            ExampleTileEntityTileEntity::new, EXAMPLE_BLOCK).build(null).setRegistryName(ExampleTileEntityTileEntity.location
+                    )
+            );
+        }
 
         @SubscribeEvent
         public static void onRegisterBlocks(RegistryEvent.Register<Block> event) {
--- src/main/java/nomowanderer/tileentity/ExampleTileEntityTileEntity.java
+++ src/main/java/nomowanderer/tileentity/ExampleTileEntityTileEntity.java
@@ -1,10 +1,44 @@
 package nomowanderer.tileentity;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraftforge.items.ItemStackHandler;
 import nomowanderer.NoMoWanderer;
 
-public class ExampleTileEntityTileEntity extends TileEntity {
+import static nomowanderer.NoMoWanderer.MODID;
+
+public class ExampleTileEntityTileEntity extends TileEntity implements ITickableTileEntity {
+    public static final String ID = "example_tile_entity_tile_entity";
+    public static ResourceLocation location = new ResourceLocation(MODID, ID);
+
     public ExampleTileEntityTileEntity() {
         super(NoMoWanderer.EXAMPLE_TILE_ENTITY);
+    }
+
+    @Override
+    public AxisAlignedBB getRenderBoundingBox() {
+        return new AxisAlignedBB(getPos(), getPos().add(1, 2, 1));
+    }
+
+    long lastChangeTime;
+    public ItemStackHandler inventory = new ItemStackHandler(1) {
+        @Override
+        protected void onContentsChanged(int slot) {
+            if (world != null && !world.isRemote) {
+                lastChangeTime = world.getGameTime();
+                BlockState state = world.getBlockState(pos);
+                world.notifyBlockUpdate(pos, state, state, 3);
+            }
+        }
+    };
+
+    @Override
+    public void tick() {
+//        if (world.isRemote) {
+//            System.out.println("BLA BLAH BLAH BLAH");
+//        }
     }
 }
