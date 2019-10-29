0a126eccaceabcc83b14e84c7b9ca0f8ce628007
Dylan <dylan.jech@mnsu.edu>
2019-10-29 17:55:17 -0500

    Now cancelling every Wandering Trader spawn

--- src/main/java/nomowanderer/EntitySpawnHandler.java
+++ src/main/java/nomowanderer/EntitySpawnHandler.java
@@ -1,8 +1,8 @@
 package nomowanderer;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.monster.CreeperEntity;
-import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.entity.merchant.villager.WanderingTraderEntity;
+import net.minecraft.world.IWorld;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,9 +12,11 @@ import net.minecraftforge.fml.common.Mod;
 public class EntitySpawnHandler {
 
     @SubscribeEvent
-    public static void onCheckSpawn(LivingSpawnEvent.CheckSpawn event) {
+    public static void onCheckSpecialSpawn(LivingSpawnEvent.SpecialSpawn event) {
         Entity entity = event.getEntity();
-        if (entity instanceof CreeperEntity || entity instanceof ZombieEntity) {
+        if (entity instanceof WanderingTraderEntity) {
+            // Cancelling the Wandering Trader spawn also cancels the Trader Llama spawns.
+            event.setCanceled( event.isCancelable() );
             event.setResult(Event.Result.DENY);
         }
     }
