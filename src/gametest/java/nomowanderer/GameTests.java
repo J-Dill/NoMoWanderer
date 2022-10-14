2ef9b01b88c0ad8de38070976fe86baaa3b3f75c
J-Dill <J-Dill@users.noreply.github.com>
2022-10-13 21:23:26 -0500

    Added GameTest for TraderRug

--- src/gametest/java/nomowanderer/GameTests.java
+++ src/gametest/java/nomowanderer/GameTests.java
@@ -14,8 +14,10 @@ import net.minecraftforge.gametest.PrefixGameTestTemplate;
 @GameTestHolder(NoMoWanderer.MODID)
 public class GameTests {
 
-    private static final BlockPos TRADER_RELATIVE_IN = new BlockPos(1, 2, 46);
-    private static final BlockPos TRADER_RELATIVE_OUT = new BlockPos(46, 2, 1);
+    private static final BlockPos SIGN_RELATIVE_IN = new BlockPos(1, 2, 46);
+    private static final BlockPos SIGN_RELATIVE_OUT = new BlockPos(46, 2, 1);
+    private static final BlockPos TRADER_SPAWN_POS = new BlockPos(0, 1, 0);
+    private static final BlockPos RUG_POS = new BlockPos(2, 2, 2);
 
     @BeforeBatch(batch = "sign")
     public static void before(ServerLevel level) {
@@ -28,7 +30,7 @@ public class GameTests {
     @PrefixGameTestTemplate(value = false)
     @GameTest(batch = "sign", template = "trader_platform")
     public static void spawnTraderInRange(GameTestHelper helper) {
-        trySpawnTrader(helper, TRADER_RELATIVE_IN);
+        trySpawnTrader(helper, SIGN_RELATIVE_IN);
         helper.assertEntityNotPresent(EntityType.WANDERING_TRADER);
         helper.succeed();
     }
@@ -36,8 +38,16 @@ public class GameTests {
     @PrefixGameTestTemplate(value = false)
     @GameTest(batch = "sign", template = "trader_platform")
     public static void spawnTraderOutOfRange(GameTestHelper helper) {
-        trySpawnTrader(helper, TRADER_RELATIVE_OUT);
+        trySpawnTrader(helper, SIGN_RELATIVE_OUT);
         helper.assertEntityPresent(EntityType.WANDERING_TRADER);
+        helper.succeed();
+    }
+
+    @PrefixGameTestTemplate(value = false)
+    @GameTest(batch = "rug", template = "trader_rug")
+    public static void spawnTraderByRug(GameTestHelper helper) {
+        trySpawnTrader(helper, TRADER_SPAWN_POS);
+        helper.assertEntityPresent(EntityType.WANDERING_TRADER, RUG_POS);
         helper.succeed();
     }
 
